f594494d63dc0ed767b034800dc5a4da062951f7
Adel <htwr86@gmail.com>
2019-06-18 12:21:46 +0300

    processing http errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, recieved ${res.status}`);
+  }
   const body = await res.json();
   return body;
 }
 
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/films/27678/')
   .then((body) => {
     console.log(body);
+  })
+  .catch((err) => {
+    console.error('Could not fetch', err);
   });
