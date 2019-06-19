3aa692bd5f3ce34782b6388507f3a6d5deec4db0
Adel <htwr86@gmail.com>
2019-06-19 22:10:14 +0300

    61.1 toggle showRandomPlanet

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -8,21 +8,37 @@ import PersonDetails from '../person-details';
 
 import './app.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+export default class App extends Component {
+
+  state = {
+    showRandomPlanet: true
+  };
+
+  onToggleShowRandomPlanet = () => {
+    this.setState ((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      };
+    });
+  };
+
+  render() {
+
+    const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
+
+    return (
+      <div>
+        <Header />
+        {planet}
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
