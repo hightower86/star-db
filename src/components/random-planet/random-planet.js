f0ab6e5750921bcda522fb75f27ebe0c0640cd58
Adel <htwr86@gmail.com>
2019-06-19 19:05:10 +0300

    58. add spinner component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -38,7 +38,7 @@ export default class RandomPlanet extends Component {
         <img className="planet-image"
              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
         <div>
-          <h4>{name}</h4>
+        <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="lds-ripple">
+      <div></div><div></div>
+    </div>
+  )
+}
+
+export default Spinner;
